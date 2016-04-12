33b9ce7a49ef827792f710390e62a3c4a0f4a978
AlejandroSA <alejandro93sa@gmail.com>
2016-04-12 11:04:22 +0200

    Removed some unused libraries.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,5 +1,5 @@
 /**
- * Created by AlejandroSA on 15/03/2016.
+ * Created by AlejandroSA on 11/04/2016.
  */
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
@@ -8,27 +8,21 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-
-import com.google.api.services.drive.Drive;
-import com.google.gdata.client.spreadsheet.SpreadsheetService;
-import com.google.gdata.data.spreadsheet.*;
-import com.google.gdata.util.AuthenticationException;
-import com.google.gdata.util.ServiceException;
-import gmailsettings.GmailSettingsService;
+import com.google.api.services.admin.directory.Directory;
+import com.google.api.services.admin.directory.model.User;
+import com.google.api.services.admin.directory.model.Users;
 
 import java.io.*;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Controller {
     /** Application name. */
     private static final String APPLICATION_NAME =
-            "AutomaticSignatures";
+            "UpdatePassword";
 
     /** Directory to store user credentials for this application. */
     private static final java.io.File DATA_STORE_DIR = new java.io.File(
@@ -44,18 +38,13 @@ public class Controller {
     /** Global instance of the HTTP transport. */
     private static HttpTransport HTTP_TRANSPORT;
 
-    /** Global instance of admin email so we can use it to compare with the email.*/
-    private static final String ADMIN_EMAIL = "asoriano@as.cloudimpulsion.com";
-
     /** Global instance of the scopes required by this quickstart.
      *
      * If modifying these scopes, delete your previously saved credentials
      * at ~/.credentials/drive-java-quickstart.json
      */
     private static final List<String> SCOPES =
-            Arrays.asList(
-                    "https://apps-apis.google.com/a/feeds/emailsettings/2.0/",
-                    "https://spreadsheets.google.com/feeds");
+            Arrays.asList("https://www.googleapis.com/auth/admin.directory.user");
 
     static {
         try {
@@ -67,8 +56,6 @@ public class Controller {
         }
     }
 
-    private static String domain_name = "as.cloudimpulsion.com";
-
     /**
      * Creates an authorized Credential object.
      * @return an authorized Credential object.
@@ -77,7 +64,7 @@ public class Controller {
     public static Credential authorize() throws IOException {
         // Load client secrets.
         InputStream in =
-                new FileInputStream("secret.json");
+                new FileInputStream("C:\\Users\\AlejandroSA\\IdeaProjects\\UpdatePasswordSecret.json");
         GoogleClientSecrets clientSecrets =
                 GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
@@ -86,7 +73,7 @@ public class Controller {
                 new GoogleAuthorizationCodeFlow.Builder(
                         HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
                         .setDataStoreFactory(DATA_STORE_FACTORY)
-                        .setAccessType("online")
+                        .setAccessType("offline")
                         .build();
         Credential credential = new AuthorizationCodeInstalledApp(
                 flow, new LocalServerReceiver()).authorize("user");
@@ -95,117 +82,32 @@ public class Controller {
         return credential;
     }
 
-    /**
-     * Build and return an authorized Drive client service.
-     * @return an authorized Drive client service
-     * @throws IOException
-     */
-
-    public static void main(String[] args) throws IOException, ServiceException {
-        // Build a new authorized API client service.
-
-        getSpreadSheetInfo(authorize());
-
-        /*try {
-            updateSignature(authorize());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+    public static Directory getDirectoryService(Credential credential) throws IOException {
+        return new Directory.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 
-    private static void getSpreadSheetInfo(Credential credential) throws IOException, ServiceException
-    {
-        SpreadsheetService service =
-                new SpreadsheetService("MySpreadsheetIntegration-v1");
+    public static void main(String[] args) throws Exception {
 
-        // We give the credentials to work using Oauth2.0
-        service.setOAuth2Credentials(credential);
+        //Get Credentials we are going to use
+        Credential credential = authorize();
 
-        // Define the URL to request.  This should never change.
-        URL SPREADSHEET_FEED_URL = new URL(
-                "https://spreadsheets.google.com/feeds/spreadsheets/private/full");
+        //Create the service to work with
+        Directory directory = getDirectoryService(credential);
 
-        // Make a request to the API and get all spreadsheets.
-        SpreadsheetFeed feed = service.getFeed(SPREADSHEET_FEED_URL,
-                SpreadsheetFeed.class);
-        List<SpreadsheetEntry> spreadsheets = feed.getEntries();
+        //Call the service to get a json result
+        Users result = directory.users().list().setCustomer("my_customer").execute();
 
-        if (spreadsheets.size() == 0) {
-            // TODO: There were no spreadsheets, act accordingly.
-        }
+        //Extract users info from the result we got calling
+        List <User> users = result.getUsers();
 
-        // Search for the needed spreadsheet where we store the signature settings
-        SpreadsheetEntry spreadsheet = null;
-
-        for (SpreadsheetEntry entry: spreadsheets) {
-            if (entry.getTitle().getPlainText().equals("TestingSignatures"))
-            {
-                spreadsheet = entry;
-            }
-        }
-        if (spreadsheet != null)
+        //Execute a put call for every user to update his password at next loggin
+        for (User user:users)
         {
-            System.out.println(spreadsheet.getTitle().getPlainText());
-
-            WorksheetFeed worksheetFeed = service.getFeed(
-                    spreadsheet.getWorksheetFeedUrl(), WorksheetFeed.class);
-            List<WorksheetEntry> worksheets = worksheetFeed.getEntries();
-            WorksheetEntry worksheet = worksheets.get(0);
-
-            // Fetch the list feed of the worksheet.
-            URL listFeedUrl = worksheet.getListFeedUrl();
-            System.out.println(listFeedUrl);
-            ListFeed listFeed = service.getFeed(listFeedUrl, ListFeed.class);
-
-
-            // Iterate through each row, printing its cell values.
-            //WARNING, It just reads from the second row until the first blank row
-
-            int column = 1;
-
-            //Creating an arraylist with 2 arraylists to introduce email at 0 and signature at 1
-            ArrayList<ArrayList<String>> mailAndSignature = new ArrayList<>();
-            mailAndSignature.add(new ArrayList<String>());
-            mailAndSignature.add(new ArrayList<String>());
-            for (ListEntry row : listFeed.getEntries()) {
-
-                // Iterate over the remaining columns, and print each cell value
-                for (String tag : row.getCustomElements().getTags()) {
-                    if (column == 1)
-                    {
-                        //Add
-                        mailAndSignature.get(0).add(row.getCustomElements().getValue(tag));
-                    }
-                    if (column == 2)
-                    {
-                        mailAndSignature.get(1).add(row.getCustomElements().getValue(tag));
-                        break;
-                    }
-                    column++;
-                }
-                column = 1;
-            }
-            System.out.println(mailAndSignature);
+            user.setChangePasswordAtNextLogin(true);
+            directory.users().update(user.getPrimaryEmail(), user).execute();
         }
-        else
-        {
-            //TODO: The specified spreadsheet is not found at the list.
-        }
-    }
-
-    private static void updateSignature(Credential credential) throws Exception {
-
-        GmailSettingsService service = new GmailSettingsService(APPLICATION_NAME, domain_name, null, null){
-            @Override
-            public void setUserCredentials(String username, String password)
-                    throws AuthenticationException {
-                // Nothing to do here, just Overriding the old method and setting it to null so we can later setOauthCredentials to the service
-            }};
-
-        service.setOAuth2Credentials(credential);
-        List users=new ArrayList();
-        users.add("abell");
-        service.changeSignature(users,
-                "This is abell signature by Alejandro");
     }
 }
